fa5af49fa1e098e12033e2dec94aba32de5820e0
Priyanshu Malaviya <87270273+Priyanshu9898@users.noreply.github.com>
2024-09-11 04:39:39 +0000

    Make changes in a merge sort code to make it running successfully.

--- hw2_debugging.py
+++ hw2_debugging.py
@@ -37,25 +37,28 @@ def recombine(left_arr, right_arr):
     left_index = 0
     right_index = 0
     merge_arr = [None] * (len(left_arr) + len(right_arr))
+
     while left_index < len(left_arr) and right_index < len(right_arr):
         if left_arr[left_index] < right_arr[right_index]:
-            right_index += 1
             merge_arr[left_index + right_index] = left_arr[left_index]
-        else:
             left_index += 1
+        else:
             merge_arr[left_index + right_index] = right_arr[right_index]
+            right_index += 1
 
-    for i in range(right_index, len(right_arr)):
-        merge_arr[left_index + right_index] = right_arr[i]
+    while right_index < len(right_arr):
+        merge_arr[left_index + right_index] = right_arr[right_index]
+        right_index += 1
 
-    for i in range(left_index, len(left_arr)):
-        merge_arr[left_index + right_index] = left_arr[i]
+    while left_index < len(left_arr):
+        merge_arr[left_index + right_index] = left_arr[left_index]
+        left_index += 1
 
     return merge_arr
 
 
 array = rand.random_array([None] * 20)
-print(array)
+
 arr_out = merge_sort(array)
 
 print(arr_out)

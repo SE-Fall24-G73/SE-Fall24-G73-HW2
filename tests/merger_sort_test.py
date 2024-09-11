64cb352663bf9b57faf9c7b797b213be75e9427f
Priyanshu Malaviya <87270273+Priyanshu9898@users.noreply.github.com>
2024-09-11 04:42:22 +0000

    Added aa testcases for merge sort for sorted array, reverse sorted array and random array to check the robustness of algorithm.

---
+++ tests/merger_sort_test.py
@@ -0,0 +1,24 @@
+from hw2_debugging import merge_sort
+
+def testcase_init_sortedArr():
+    """
+    Testcase to check if the function can handle an already sorted array.
+    """
+    arr = [1, 2, 3, 4, 5]
+    assert merge_sort(arr) == [1, 2, 3, 4, 5], "Testcase 1 Passed"
+
+
+def testcase_init_reverse_sortedArr():
+    """
+    Testcase to check if the function can handle a reverse sorted array.
+    """
+    arr = [5, 4, 3, 2, 1]
+    assert merge_sort(arr) == [1, 2, 3, 4, 5], "Testcase 2 Passed"
+
+
+def testcase_init_randomArr():
+    """
+    Testcase to check if the function can handle a random array.
+    """
+    arr = [3, 1, 5, 2, 4]
+    assert merge_sort(arr) == [1, 2, 3, 4, 5], "Testcase 3 Passed"
